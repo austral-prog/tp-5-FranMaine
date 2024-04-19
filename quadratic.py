ce5f69005538e89c5e4e096d604b721eb0e3eefb
FranMaine <162723272+FranMaine@users.noreply.github.com>
2024-04-19 14:46:01 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,31 @@
-# Replace the "ANSWER HERE" for your answer
-
 def roots(a, b, c):
-    return "ANSWER HERE"
+    import math
+    discriminant= b**2 - 4*a*c
+    if discriminant>0:
+        r1 = (-b + math.sqrt(discriminant)) / 2*a
+        r2 = (-b - math.sqrt(discriminant)) / 2*a
+        return f"({r1}, {r2})"
+    elif discriminant == 0:
+        r = -b / (2*a)
+        return f"({r})"
+    else:
+        return "( )"
 
-
+raiz= roots(1,-3,2)
+print(raiz)
+    
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
-
+    y= a*(x**2) + b*x + c
+    return y
+cuad= value_y(1,-3,2,0)
+print(cuad)
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    return f"f(x) = {a} * X^2 + {b} * X + {c}"
+string= to_string(2,-3,1)
+print(string)
 
-
-def derivation(a, b):
-    return "ANSWER HERE"
+def derivation(a, b, c):
+    return f"f'(x) = {2*a}X + {b}"
+deriv=derivation(2,-3,1)
+print(deriv)
